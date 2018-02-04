394c7f3d33d687c91367c58f464e6f799292e40d
Einstrasse <hg9587@naver.com>
2018-02-05 02:35:47 +0900

    hls서버와 웹서버 좀 다듬음. 브라우저에서 마이크를 통한 음성녹음 일부 구현

--- index.js
+++ index.js
@@ -2,89 +2,79 @@ const HLSServer = require('hls-server')
 const http = require('http')
 const fs = require('fs')
 const url = require('url')
+const path = require('path')
 
 const audioPath = __dirname + "/audio/";
 const viewPath = __dirname + "/view/";
 
-let PORT = 8000;
+let PORT = process.env.PORT || 8000;
 let SERVER_ADDR = "127.0.0.1"
 var server = http.createServer(function(req, res) {
 	var uri = url.parse(req.url).pathname;
 	var ext = uri.split('.').pop();
+	//hls 미들웨어를 통과한 요청을 처리함. 웹 서버 역할을 함.
 	if (uri == '/') {
+		console.log(uri);
+		res.writeHead(302, {
+			'Location': '/play_demo'
+		});
+		res.end();
+		return;
+	} else if (uri == '/play_demo') {
+		console.log(uri);
 		res.writeHead(200, {
 			'Content-Type': 'text/html'
 		});
-		var stream = fs.createReadStream(viewPath + "index.html");
+		var stream = fs.createReadStream(viewPath + "play_demo.html");
 		stream.pipe(res);
-		// var html = [
-		// 	'<!doctype html>',
-		// 	'<html>',
-		// 		'<head>',
-		// 			'<title> Einstrasse HLS Audio Live Streaming Player </title>',
-		// 		'</head>',
-		// 		'<body>',
-		// 			'<video src="http://', SERVER_ADDR + ':' + PORT + '/manifest.m3u8" controls autoplay>',
-		// 		'</body>',
-		// 	'</html>'
-		// ].join('');
-		// res.write(html);
-		// res.end();
 		return;
-	} else if (ext == 'm3u8') {
-		return fs.readFile(audioPath + 'manifest.m3u8', function(err, contents) {
-			if (err) {
-				res.writeHead(500);
-				res.end();
-			} else if (contents) {
-				res.writeHead(200, {
-					'Content-Type': 'application/vnd.apple.mpegurl'
-				});
-				res.end(contents, 'utf-8');
-			}
-		});
-	} else if (ext == 'ts') {
-		var filePath = audioPath + uri;
-		var exists = fs.existsSync(filePath);
-		if (!exists) {
-			res.writeHead(404);
-			return res.end();
-		}
+	} else if (uri == '/capture_audio') {
+		console.log(uri);
 		res.writeHead(200, {
-			'Content-Type': 'video/MP2T'
-		});
-		var stream = fs.createReadStream(filePath);
+			'Content-Type': 'text/html'
+		})
+		var stream = fs.createReadStream(viewPath + "capture_audio.html");
 		stream.pipe(res);
 		return;
 	}
-	
 })
-// var hls = new HLSServer(server, {
+var hls = new HLSServer(server, { // hls를 위한 미들웨어가 삽입됨. hls 서버 역할을 함
+	provider: {
+		exists: function(req, cb) { // 모든 요청에 대해 호출됨
+			var uri = url.parse(req.url).pathname
+			var extension = path.extname(uri)
+			if (extension != '.m3u8' && extension != '.ts') {
+				return cb(null, true);
+			}
+			console.log('exists function called');
+			var uri = url.parse(req.url).pathname;
+			var fpath = audioPath + uri;
+			fs.access(fpath, fs.constants.F_OK, function(err) {
+				if (err) {
+					console.log('File not exist', fpath);
+				}
+				cb(null, !err);
+			});
+		},
+		getManifestStream: function(req, cb) { // 확장자가 .m3u8 로 끝나는 경우 호출됨
+			var uri = url.parse(req.url).pathname;
+			console.log('getManifestStream function called', req.url, uri)
+			var manifestStream = fs.createReadStream(audioPath + 'manifest.m3u8');
+			cb(null, manifestStream);
+		},
+		getSegmentStream: function(req, cb) { // 확장자가 .ts 로 끝나는 경우 호출됨
+			var uri = url.parse(req.url).pathname;
+			console.log('getSegmentStream function called:', req.url, uri);
+			var ext = uri.split('.').pop();
+			if (ext == 'ts') {
+				var filePath = audioPath + uri;
+				var tsFileStream = fs.createReadStream(filePath);
+				cb(null, tsFileStream);
+			}
+		}
+	}
+})
 
-// 	// path: '/stream',
-// 	// dir: 'audio'
-// 	provider: {
-// 		exists: function(req, cb) {
-// 			console.log('exists function called');
-// 			cb(null, true);
-// 		},
-// 		getManifestStream: function(req, cb) { // ~/asdf.m3u8 request handler
-// 			console.log('getManifestStream function called')
-// 			var manifestStream = fs.createReadStream(audioPath + 'manifest.m3u8');
-// 			cb(null, manifestStream);
-// 		},
-// 		getSegmentStream: function(req, cb) { // ~/asdf.ts request handler
-// 			console.log('getSegmentStream function called');
-// 			var uri = url.parse(req.url).pathname;
-// 			var ext = uri.split('.').pop();
-// 			if (ext == 'ts') {
-// 				var filePath = audioPath + uri;
-// 				var tsFileStream = fs.createReadStream(filePath);
-// 				cb(null, tsFileStream);
-// 			}
-			
-// 		}
-// 	}
-// })
-
-server.listen(PORT)
+server.listen(PORT, function() {
+	console.log('Launching server listening on ... ', SERVER_ADDR + ':' + PORT);
+})
